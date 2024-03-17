4921dfec4a897d225fbdbb99f044c259d3c6cc5d
Oh...Yeah!!! <614988210@qq.com>
2024-03-17 21:35:08 +0800

    完善配置

--- guide/src/main/java/com/gsxy/guide/mapper/GuideMapper.java
+++ guide/src/main/java/com/gsxy/guide/mapper/GuideMapper.java
@@ -25,15 +25,6 @@ public interface GuideMapper {
     Guide queryById(Long id);
 
     /**
-     * 查询指定行数据
-     *
-     * @param guide 查询条件
-     * @param pageable         分页对象
-     * @return 对象列表
-     */
-    List<Guide> queryAllByLimit(Guide guide, @Param("pageable") Pageable pageable);
-
-    /**
      * 统计总行数
      *
      * @param guide 查询条件
@@ -49,22 +40,6 @@ public interface GuideMapper {
      */
     int insert(Guide guide);
 
-    /**
-     * 批量新增数据（MyBatis原生foreach方法）
-     *
-     * @param entities List<Guide> 实例对象列表
-     * @return 影响行数
-     */
-    int insertBatch(@Param("entities") List<Guide> entities);
-
-    /**
-     * 批量新增或按主键更新数据（MyBatis原生foreach方法）
-     *
-     * @param entities List<Guide> 实例对象列表
-     * @return 影响行数
-     * @throws org.springframework.jdbc.BadSqlGrammarException 入参是空List的时候会抛SQL语句错误的异常，请自行校验入参
-     */
-    int insertOrUpdateBatch(@Param("entities") List<Guide> entities);
 
     /**
      * 修改数据
